ce80ea87948ebc7ff5cdf09628430b27b6731331
Anup Nayak <godwin.mr.cool@gmail.com>
2017-12-07 12:28:11 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  # get 'pages/home'
 
-  get 'pages/contact'
+  # get 'pages/contact'
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  root :to => 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
