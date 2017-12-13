7394fbdbd0040e9ec42bbbf368a90099448fd9bf
Anup Nayak <godwin.mr.cool@gmail.com>
2017-12-13 12:17:48 +0530

    User model with password and user show page

--- db/migrate/20171212112116_add_password_to_users.rb
+++ db/migrate/20171212112116_add_password_to_users.rb
@@ -1,5 +1,6 @@
 class AddPasswordToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :encrypted_password, :string
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212113456) do
+ActiveRecord::Schema.define(version: 20171213061247) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.string   "encrypted_password"
     t.string   "salt"
+    t.string   "password"
+    t.string   "password_confirmation"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
