c2c4a5e03d7d420cae8fcf8d0bd79b44c7c5cf0c
Anup Nayak <godwin.mr.cool@gmail.com>
2017-12-14 17:30:28 +0530

    User signup

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 require_relative 'boot'
 
+require File.expand_path('../boot', __FILE__)
+
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -12,6 +14,10 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,5 +27,8 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password, :password_confirmation]
   end
 end
